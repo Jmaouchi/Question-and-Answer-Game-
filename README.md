1326e4fc6c0e7234730ec957f31530e4d4bbc6ac
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-03-04 22:20:27 -0600

    Added all the listeners and made the code readable

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Question-and-Answer-Game-
+# Question-and-Answer-Game
+Purpose
+A question Game Quiz will allow you answer questions that are realated to some coding program languages.
+1- will go to the next question when you answer a question 
+
+2- will display correct or wrong while clicking an answer 
+
+3- will update timer when answer is wrong 
+
+4- will give you score whene you finish answering the questions 
+
+# Built With
+- HTML
+- CSS
+- Portfolio
+
+Contribution
+Made by [Jugurta Maouchi] 2022.
+
+©️2022 Quiz, Inc
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
-var btn = document.getElementById('section-one-content');
-var span = document.getElementById('show-answer');
+var selectAnswer = document.getElementById('section-one-content');
+var answerType = document.getElementById('answer-type');
 var countdown = document.getElementById('countdown');
 var yourScore = document.getElementById('your-score');
 var scoreResult= document.getElementById('result'); 
@@ -8,23 +8,34 @@ var closeBtn = document.getElementById('close-btn');
 var checkScore = document.getElementById('checkScore');
 var playAgain = document.getElementById('play-again');
 var score = 0;
-var timer =5;
+var timer = 75;
 var objectId = 0;
 var scoreArray = [];
 
 // creating a question object with different answers
 var questions = [
   {
-    question : 'How many states in the USA',
-    answerOne : 'fifty',
-    answerTwo : 50,
-    answerThree : 43,
-    answerFour : 65,
+    question : 'What is HTML?',
+    answerOne : 'HTML stands for Hyper Text Markup Language',
+    answerTwo : 'HTML is a programing language used to style and layout web pages',
+    answerThree :'HTML is a JavaScript freamwork',
+    answerFour : 'None of the above',
 
-    rightAnswer : 'fifty'
+    rightAnswer : 'HTML stands for Hyper Text Markup Language'
   },
+
   {
-    question : 'what is JavaScript',
+    question : 'Whats the main use of CSS?',
+    answerOne : 'Display Content in a web-page',
+    answerTwo : 'style and layout web pages',
+    answerThree :'Css will alow us to add click events to buttons',
+    answerFour : 'None of the above',
+
+    rightAnswer: 'style and layout web pages'
+  },
+
+  {
+    question : 'what is JavaScript?',
     answerOne : 'A programing Language',
     answerTwo : 'A soccer place',
     answerThree : 'A restaurant',
@@ -32,29 +43,43 @@ var questions = [
 
     rightAnswer: 'A programing Language'
   },
-  {
-    question : 'What is the biggest state in the USA',
-    answerOne : 'Texas',
-    answerTwo : 'Ohio',
-    answerThree : 'Alaska',
-    answerFour : 'Califorian',
 
-    rightAnswer: 'Alaska'
+  {
+    question : 'Why so JavaScript and Java similar name?',
+    answerOne : 'JavaScript is a stripped-down version of java',
+    answerTwo : "JavaScript's syntax is looseley based on java's",
+    answerThree : 'They both originated on the island of java',
+    answerFour : 'None of the above',
+
+    rightAnswer: "JavaScript's syntax is looseley based on java's"
   },
-  {
-    question : 'where is algeria located at',
-    answerOne : 'Africa',
-    answerTwo : 'Asia',
-    answerThree :'europe',
-    answerFour : 'IDK',
 
-    rightAnswer: 'Africa'
+  {
+    question : '___ JavaScript is also called client-side JavaScript?',
+    answerOne : 'Microsoft',
+    answerTwo : 'Navigator',
+    answerThree :'LiveWire',
+    answerFour : 'Native',
+
+    rightAnswer: 'LiveWire'
+  },
+
+  {
+    question : 'What are variables used for in javaScript programs?',
+    answerOne : 'Storing numbers, dates, or other values',
+    answerTwo : 'Varying randomly',
+    answerThree :'Causing hight-school algebra flashbacks',
+    answerFour : 'None of the above',
+
+    rightAnswer: 'What are variables used for in javaScript programs'
   },
 
 ]
 
+//set curent item to 0 and it will decrease everytime the answer will be right
 let currentItem = 0;
 
+//for loop to display all the questions
 for(let i=0 ; i<questions.length; i++ ){
   function showQuestion(){
     item = questions[currentItem];  
@@ -76,14 +101,14 @@ function updateCountDown(){
   countdown.innerHTML = timer;
   timer--;
 
-  if(timer <= 0){ // i added the < because whenever i check the wrong answer, the timer might skip the 0 and go to the negative number and my modalOverlay wont show
-    //clear Interval whenever timer will be 0
+  if(timer <= 0){ // I added the <  because whenever i check the wrong answer, the timer might skip the 0 and go to the negative number and my modalOverlay wont show
+    //clear Interval whenever timer will be 0, and 
     clearInterval(setTime);
     // this will go to the score section whenever the score will be equal to 0
     modalOverlay.style.visibility = 'visible';
     var scoreText = document.createElement ('p');
     scoreText.className = 'score-text';
-    scoreText.innerHTML = currentItem;
+    scoreText.innerHTML = `${score} / ${questions.length}`;
     checkScore.appendChild(scoreText);
     //creat a play again button
     var buttonPlay = document.createElement ('button');
@@ -92,49 +117,42 @@ function updateCountDown(){
     buttonPlay.textContent = 'Play Again'
     playAgain.appendChild(buttonPlay);
 
-    
-    //set inital to local storage
-    scoreArray.push(currentItem);
-    localStorage.setItem('score', JSON.stringify (scoreArray));
-
-    var getScoreN =JSON.parse(localStorage.names);
-    localStorage.getItem(getScoreN);
   }
-
-
-  // if (?? === questions.length){
-  //  need to reset my timer
-  // }
-
 }
 
- // Change the content of the questions and answers by going trough the array objects 
+ // Change the content of the questions and answers by going trough the array objects and that with calling the showQuestion function
  var answerQuestions = function(e){
-    // if thecurrentItem is false then increament it by one
-    rightAnswer = questions[currentItem].rightAnswer
     if (e.target.textContent !== questions[currentItem].rightAnswer){
       //decrease the timer by 5s
       timer = timer - 5;
       //display a wrong text with a red background color 
-      span.innerHTML = 'wrong';
-      span.style.fontSize = '32px';
-      span.style.backgroundColor = 'red';
+      answerType.innerHTML = 'wrong';
+      answerType.style.fontSize = '32px';
+      answerType.style.backgroundColor = 'red';
       // display the class that we did hide in our css
-      span.style.display = 'block';
+      answerType.style.display = 'block';
      }
      else{
       //display a wrong text with a green background color 
       score++;
-      yourScoreValue = yourScore;
-      span.innerHTML = 'correct';
-      span.style.fontSize = '32px';
-      span.style.backgroundColor = 'green';
+      answerType.innerHTML = 'correct';
+      answerType.style.fontSize = '32px';
+      answerType.style.backgroundColor = 'green';
       // display the class that we did hide in our css
-      span.style.display = 'block';
+      answerType.style.display = 'block';
      }
     
-    currentItem++;
-    showQuestion();
+    if(objectId === questions.length){
+      timer = 0;
+          
+      //set inital to local storage
+      scoreArray.push(score);
+      localStorage.setItem('score', JSON.stringify (scoreArray));
+
+      } else {
+      currentItem++;
+      showQuestion();
+      }
  }
 
 
@@ -143,15 +161,22 @@ function updateCountDown(){
     modalOverlay.style.visibility = 'visible';
   }
 
+// close the score section whenever you click the red button
   var closeScoreSection = function (){
     modalOverlay.style.visibility = 'hidden';
   }
-
 
 //play again function
   var anotherRound = function() {
     window.location.reload();
   }
+
+// get score form LS whenever you reload the page 
+var getScoreFromLs = function(){
+  var scoreArrayFromLs = localStorage.getItem('score');
+  console.log(`checking score array from ls: ${scoreArray}`);
+  console.log(`checking last index on the scoreArray from ls: ${scoreArrayFromLs}`);
+}
 
 
 
@@ -159,5 +184,6 @@ function updateCountDown(){
 playAgain.addEventListener('click', anotherRound); // I set this function to reload the page even that i set a http path on the HTML page 
 scoreResult.addEventListener('click', getScoreResult);
 closeBtn.addEventListener('click', closeScoreSection);
-btn.addEventListener('click', answerQuestions);
+selectAnswer.addEventListener('click', answerQuestions);
 window.addEventListener("DOMContentLoaded", showQuestion);
+window.addEventListener("DOMContentLoaded", getScoreFromLs);
